384d84f633588ae479acf32cc3c159b5b0dabbe5
gungunjain36 <jgungun1723@gmail.com>
2024-12-24 08:49:47 +0530

    fe: frontend init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+   <div className='bg-pink-200 text-blue-500'>
+    <h1 className="text-blue-600">Hello World</h1>
+   </div>
+  )
+}
+
+export default App
